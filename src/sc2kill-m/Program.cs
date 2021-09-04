f33f5fce1023e602005f8dac512b243f7c5ab8b5
confessore <confessore@balasolu.com>
2021-09-04 16:59:36 -0400

    Add project files.

---
+++ src/sc2kill-m/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace sc2kill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var process = Process.GetProcesses().FirstOrDefault(x => x.ProcessName.Contains("SC2_x64"));
+            if (process != null)
+                process.Kill();
+        }
+    }
+}
